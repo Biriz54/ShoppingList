3dfe09bd8352c313dbdefe6588e3dff022d9ed0f
Biriz54 <99189168+Biriz54@users.noreply.github.com>
2022-09-28 13:08:25 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,92 @@
-import React, { useState } from "react";
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faplus } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {v4 as uuidv4 } from "uuid";
 
+const App = () => {
 
-function RenderItemsList() {
-  const itemsList = [{
-    item: 'bananas',
-    quantity: 2,
-    crossed: false,
-  }];
-    const plusSign = <FontAwesomeIcon icon={faplus} />
-  const list = itemsList.map((e) => {
-      return (
-        <ul>
-          <li>{e.item}<button>{plusSign}</button>{e.quantity}<button>-</button><button>del</button></li>
-        </ul>
-      )
-    }
-  )
+const [items, setItems] = useState([
+  { id: uuidv4(), itemName: 'bananas', quantity: 3, isSelected: false}
+]);
+
+const [inputValue, setInputValue] = useState('');
+
+const handleAddButtonClick = () => {
+  const newItem = {
+    id: uuidv4(),
+    itemName: inputValue,
+    quantity: 1,
+    isSelected: false,
+  };
+  const newItems = [...items, newItem];
+
+  setItems(newItems);
+  setInputValue('');
+};
+const handleQuantityIncrease = (index) => {
+  const newItems = [...items];
+
+  newItems[index].quantity++;
+
+  setItems(newItems);
+};
+const handleQuantityDecrease = (index) => {
+  const newItems = [...items];
+  newItems[index].quantity--;
+  setItems(newItems);
+};
+const toggleComplete = (index) => {
+  const newItems = [...items];
+  newItems[index].isSelected = !newItems[index].isSelected;
+  setItems(newItems);
+};
+const handleDeleteItem = (id) => {
+  const newItems = [...items];
+  
+  setItems(newItems.filter((el) => el.id !== id));
+
+}; 
+
   return (
-    <div>
-      {list}
+    <div className='app-background'>
+			<div className='main-container'>
+				<div className='add-item-box'>
+					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
+				</div>
+				<div className='item-list'>
+					{items.map((item, index) => (
+						<div className='item-container'>
+							<div className='item-name' onClick={() => toggleComplete(index)}>
+								{item.isSelected ? (
+									<>
+										<span key={item.id} className='completed'>{item.itemName}</span>
+									</>
+								) : (
+									<>
+										<span key={item.id}>{item.itemName}</span>
+									</>
+								)}
+							</div>
+							<div className='quantity'>
+								<button>
+									<FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityDecrease(index)} />
+								</button>
+								<span> {item.quantity} </span>
+								<button>
+									<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
+								</button>
+                <button>
+                <FontAwesomeIcon icon={faTrash} onClick={() => handleDeleteItem(item.id)} />
+                </button>
+							</div>
+						</div>
+					))}
+				</div>
+		</div>
     </div>
-  )
-}
-class ShoppingList extends React.Component {
-  render() {
-    return (
-      <div className="listApp">
-    <h1>Shopping List</h1>
-  <form>
-    <label>Add Stuff:<br />
-    <input type="text" /> 
-    <button className="addBtn">Add</button>
-    </label>
-  </form>
-  <div className="listItems">
-    <RenderItemsList />
-  </div>
-  </div>
-    );
-  }
-}
+	);
+};
 
-export default ShoppingList;
+export default App;
